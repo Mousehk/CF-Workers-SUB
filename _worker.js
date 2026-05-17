4be18cda946891d03022bbfec875f796d39faca3
Mousehk <244761986@qq.com>
2026-05-17 13:52:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ https://cfxr.eu.org/getSub
 `;
 
 let urls = [];
-let subConverter = "api.asailor.org"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConverter = "url.v1.mk"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subConfig = "https://raw.githubusercontent.com/Mousehk/clash/main/Clash-mini.ini"; //订阅配置文件
 let subProtocol = 'https';
 
